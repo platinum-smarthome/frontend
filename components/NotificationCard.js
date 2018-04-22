d08057c8a625868f67d93f4a720dd82d575670c3
herby herado <herby.herado@gmail.com>
2018-04-22 14:43:32 +0700

    display image in notif done

--- components/NotificationCard.js
+++ components/NotificationCard.js
@@ -15,6 +15,11 @@ export default class NotificationCard extends Component {
           <Text style={styles.notifTitle}>{ title }</Text>
         </View>
         <Text style={styles.time}>{ dateDisplayFormater(createdAt) }</Text>
+        { this.props.data.imageUrl &&
+          <View style={{alignItems: 'center'}}>
+            <Image style={{width: 120, height: 120}} source={{ uri: this.props.data.imageUrl }} />
+          </View>
+        }
         <Text style={styles.descText}>{ description }</Text>
         <NotificationCardFooter
           onPress={ () => notificationsDelete(id) }
