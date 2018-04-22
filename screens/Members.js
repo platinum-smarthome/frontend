f63d806800aef2c4a661efd41a977fb56bf44b40
herby herado <herby.herado@gmail.com>
2018-04-22 17:19:09 +0700

    add animation on members

--- screens/Members.js
+++ screens/Members.js
@@ -3,6 +3,7 @@ import { View, Text, StyleSheet, Image, FlatList } from 'react-native';
 import { connect } from 'react-redux'
 import Member from '../components/Member'
 import PinText from '../components/PinText'
+import * as Animatable from 'react-native-animatable';
 
 class Members extends Component {
 
@@ -25,11 +26,13 @@ class Members extends Component {
         <Image style={{ marginTop: 20, width: 120, height: 120}} source={require('../components/assets/familia.png')} />
         <PinText text={'Members List'}/>
       </View>
+      <Animatable.View animation="zoomInUp" easing="ease-in-out" >
       <FlatList 
         data={ arr }
         keyExtractor= { this.keyExtractor }
         renderItem={ this.renderItem }
       />
+      </Animatable.View>
     </View>
     )
   }
